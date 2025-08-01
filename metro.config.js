d15c41de08271d9f809a92cf1d3e0fb37b6a2464
byrongodoy <desarrollador11@jaher.com.ec>
2025-08-01 09:34:15 -0500

    FIX: CONFIGURACIONES

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,15 @@
 const { getDefaultConfig } = require("expo/metro-config");
+
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Asegura que resolver existe
+config.resolver = config.resolver || {};
+config.resolver.sourceExts = config.resolver.sourceExts || [];
 config.resolver.sourceExts.push("sql");
+
+// Asegura que transformer existe y agrega el plugin de assets
+config.transformer = config.transformer || {};
+config.transformer.assetPlugins = ["expo-asset/tools/hashAssetFiles"];
+
 module.exports = config;
