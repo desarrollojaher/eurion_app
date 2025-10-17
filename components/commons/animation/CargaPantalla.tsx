cd131cf1beb2a1df281752a0ddf65cf81bfa05d9
byrongodoy <desarrollador11@jaher.com.ec>
2025-10-17 16:25:17 -0500

    FIx loading

--- components/commons/animation/CargaPantalla.tsx
+++ components/commons/animation/CargaPantalla.tsx
@@ -1,11 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Animated, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useRef } from "react";
 import { AZUL } from "@/constants/Colors";
 
 const CargaPantalla = () => {
+  const scaleAnim = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(scaleAnim, {
+          toValue: 1.2,
+          duration: 500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(scaleAnim, {
+          toValue: 1,
+          duration: 500,
+          useNativeDriver: true,
+        }),
+      ]),
+    ).start();
+  }, [scaleAnim]);
   return (
     <View style={styles.panta}>
-      <Text>CargaPantalla</Text>
+      <View style={styles.container}>
+        <Animated.Image
+          source={require("../../../assets/images/splash-icon.png")}
+          style={[styles.imagen, { transform: [{ scale: scaleAnim }] }]}
+          resizeMode="contain"
+        />
+      </View>
+      <Text style={{ color: "white" }}>ERION APP</Text>
     </View>
   );
 };
@@ -18,5 +43,14 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: AZUL,
+  },
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imagen: {
+    width: 200,
+    height: 200,
+    transform: [{ scale: 1.5 }], // Escala 1.5x
   },
 });
