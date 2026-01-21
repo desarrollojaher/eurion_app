bef5bd614e8331dcf9bff71cb37737be55dc6a17
byrongodoy <desarrollador11@jaher.com.ec>
2026-01-21 11:40:14 -0500

    FIX CERTIFICACION

--- api/config.ts
+++ api/config.ts
@@ -2,10 +2,10 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://172.17.28.6:5133/api/eurionapp",
+  //baseURL: "http://172.17.28.6:5133/api/eurionapp",
   // baseURL: "http://localhost:5133/api-eurionapp",
   // baseURL: "https://app.jmsg.ec/bedapi/eurionapp",
-  // baseURL: "https://appcert.jmsg.ec/api/eurionapp",
+  baseURL: "https://appcert.jmsg.ec/api/eurionapp",
   timeout: 180000,
 });
 
