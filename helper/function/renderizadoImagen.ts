a241912efcc3c6a82374598412a0655a1a18379f
byrongodoy <desarrollador11@jaher.com.ec>
2026-02-09 08:59:27 -0500

    fix render versiion

--- helper/function/renderizadoImagen.ts
+++ helper/function/renderizadoImagen.ts
@@ -1,13 +1,23 @@
 import { Dimensions } from "react-native";
 
 export const convertirTamanoHorizontal = (proporcion: number) => {
-  const { width } = Dimensions.get("window");
-  const BASE_WIDTH = 412; // ancho de referencia para el diseño
-  return Math.round((proporcion / BASE_WIDTH) * width);
+  const { width, height } = Dimensions.get("window");
+  const diagonal = Math.sqrt(width * width + height * height);
+  const BASE_WIDTH = 412;
+  const BASE_HEIGHT = 917;
+  const diagonalBase = Math.sqrt(
+    BASE_WIDTH * BASE_WIDTH + BASE_HEIGHT * BASE_HEIGHT,
+  );
+  return Math.round((proporcion / diagonalBase) * diagonal);
 };
 
 export const convertirTamanoVertical = (proporcion: number) => {
-  const { height } = Dimensions.get("window");
-  const BASE_HEIGHT = 917; // alto de referencia para el diseño
-  return Math.round((proporcion / BASE_HEIGHT) * height);
+  const { width, height } = Dimensions.get("window");
+  const diagonal = Math.sqrt(width * width + height * height);
+  const BASE_WIDTH = 412;
+  const BASE_HEIGHT = 917;
+  const diagonalBase = Math.sqrt(
+    BASE_WIDTH * BASE_WIDTH + BASE_HEIGHT * BASE_HEIGHT,
+  );
+  return Math.round((proporcion / diagonalBase) * diagonal);
 };
