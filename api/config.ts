9c75e62cbd1a98f093eba58a29f19dc4414e1489
byrongodoy <desarrollador11@jaher.com.ec>
2026-04-14 17:33:41 -0500

    fix baseurl

--- api/config.ts
+++ api/config.ts
@@ -17,7 +17,7 @@ const createAxiosInstance = async () => {
   const token = await AsyncStorage.getItem("token");
 
   const instance = axios.create({
-    baseURL: "http://172.17.28.14:5133/api/eurionapp",
+    baseURL: "https://apiscoreprod.jmsg.ec/api-eurionapp",
     timeout: 90000, // mucho mejor que 180000
     headers: {
       Connection: "close", // fuerza nueva conexión
